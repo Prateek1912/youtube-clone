86999a786bd1cf6b37274d5454fdefb9d291973f
Prateek Joshi <63448924+Prateek1912@users.noreply.github.com>
2024-12-23 19:40:14 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const YOUTUBE_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=AIzaSyAcU3UqZNWr5dFiavdR9wRcA-lUWfzxxmw";
+  "";
 
 export const YOUTUBE_SEARCH_API =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  "";
 
 export const OFFSET_LIVE_CHAT = 10;
